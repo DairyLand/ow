4493b1c0ee521ddd2a18b057d0786f10eae7a8ff
Hunter <farmerhunter@proton.me>
2023-02-16 13:54:03 -0500

    async

---
+++ main.py
@@ -0,0 +1,22 @@
+import asyncio
+import os
+import random
+import discord
+from discord.ext import commands
+from discord_components.client import DiscordComponents
+from discord import Embed
+import asyncpraw
+from random import random
+
+bot = commands.Bot(command_prefix='$')
+ownerid = []
+botowner = 'ThiccDaddy#1750'
+bot.remove_command('help')
+
+
+reddit = asyncpraw.Reddit(client_id='WirWugQJMWZrR8GsZGosEg',
+                          client_secret='ZGaCRxEl0OBrVdbeti_6Xn81YUXYlg',
+                          username='No-Giraffe-691',
+                          password='fakepass',
+                          user_agent='ow_bot'
+                          )
