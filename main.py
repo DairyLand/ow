aaa2fad20b0f0eb4caa71805c8186b13f41d90ea
Hunter <farmerhunter@proton.me>
2023-02-16 14:10:59 -0500

    added a bot command

--- main.py
+++ main.py
@@ -29,3 +29,27 @@ async def gen_memes():
     subreddit = await reddit.subreddit('Overwatch_Memes')
     top = subreddit.top(limit=500)
     async for submission in top:
+        all_subs.append(submission)
+
+
+@bot.event
+async def on_ready():
+    bot.loop.create_task(status_task())
+    print('Fired up & ready!')
+    DiscordComponents(bot)
+    await gen_memes()
+
+
+@bot.command(aliases=['memes'])
+async def meme(ctx):
+    random_sub = random.choice(all_subs)
+    all_subs.append(random_sub)
+    name = random_sub.title
+    url = random_sub.url
+    likes = random_sub.score
+    comments = random_sub.num_comments
+    link = random_sub.permalink
+
+    embed = Embed(title=f'__{name}__', colour=discord.Colour.random(
+    ), timestamp=ctx.message.created_at, url=f'https://reddit.com{link}')
+    embed.set_author
